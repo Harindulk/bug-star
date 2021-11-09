2c486404c8c5d1ac9e4cb838262f14cf0a0081e8
Harindu Fonseka <harindulakshitha525@gmail.com>
2021-11-09 22:34:14 +0530

    11/09

---
+++ Assets/Creator Kit - FPS/Scripts/System/EndCheckpoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndCheckpoint : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Controller>() == null)
+            return;
+        
+        
+        GameSystem.Instance.StopTimer();
+        GameSystem.Instance.FinishRun();
+        Destroy(gameObject);
+    }
+}
