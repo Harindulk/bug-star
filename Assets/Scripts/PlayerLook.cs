0218fbbf8ff80531c725c0046c0a5cc7c52ba3e2
Harindu Fonseka <harindulakshitha525@gmail.com>
2021-11-14 23:03:08 +0530

    Update PlayerLook.cs

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -1,18 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRotation;
+    float yRotation;
+
+    private void Start()
     {
-        
+        cam = GetComponentInChildren<Camera>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MyInput();
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);
+    }
+
+    void MyInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRotation += mouseX * sensX * multiplier;      
+        xRotation -= mouseY * sensY * multiplier; 
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
     }
 }
