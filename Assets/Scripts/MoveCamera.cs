d673097926cf896fcb8d86394287954f2343baaa
Harindu Fonseka <harindulakshitha525@gmail.com>
2021-11-14 23:03:02 +0530

    Create MoveCamera.cs

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    [SerializeField] Transform cameraPosition;
+    void Update()
+    {
+       
+    }
+}
