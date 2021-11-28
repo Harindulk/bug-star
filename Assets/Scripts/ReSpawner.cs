155f7c1763b2c2fc52466bfe7028e4e8e84a370f
Harindu Fonseka <harindulakshitha525@gmail.com>
2021-11-28 22:52:01 +0530

    update

--- Assets/Scripts/ReSpawner.cs
+++ Assets/Scripts/ReSpawner.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Respawn : MonoBehaviour
+public class ReSpawner : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
