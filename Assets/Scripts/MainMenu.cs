0a877ce3a84fc596331135299eee9f63ceb197ea
Harindu Fonseka <harindulakshitha525@gmail.com>
2021-12-01 01:34:54 +0530

    12/01 | 1:34AM
    update full project

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void SelectToPlay()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+}
